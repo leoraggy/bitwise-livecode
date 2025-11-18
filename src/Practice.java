d3c513c3ff15dc5b632870106441247e71bf1a84
leoraggy <leoragual35@gmail.com>
2025-11-18 14:51:31 -0800

    Finished bitwise assignment

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,26 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Yay bitwise operations!");
+        int x = 21;
+        int p = 0b11001;
+
+        int color = 0xc4246D;
+        int blueMask = 0x00FF00;
+
+        int blue = color & blueMask;
+
+        int greenMask = 0x00FF00;
+
+        int green = (color & greenMask) >> 8;
+
+        System.out.println(Integer.toHexString(green));
+
+        int q = x & p;
+
+        System.out.println(Integer.toBinaryString(q));
+
+        int y = 0xffff;
+        int z = 94;
+        
+        System.out.println(Integer.toHexString(z));
     }
 }
